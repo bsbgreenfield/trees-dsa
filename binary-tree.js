cdd85a46290e0c59060181a8368805db85a84dc4
Benjamin Greenfield <benjamingreenfield@Benjamins-MacBook-Pro.local>
2023-05-08 15:19:16 -0700

    add functions

--- binary-tree.js
+++ binary-tree.js
@@ -16,29 +16,94 @@ class BinaryTree {
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
+  
   minDepth() {
+    if (!this.root) return 0;
+   function findMinDepth(node){
+    if (!node.left && !node.right){
+      return 1
+    }
+    if (node.left === null) return findMinDepth(node.left) + 1
+    if (node.right === null) return findMinDepth(node.right) +1
+    return (
+      Math.min(findMinDepth(node.left), findMinDepth(node.right)) + 1
+    )
+   }
 
+   return findMinDepth(this.root)
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
-
+    if (!this.root) return 0;
+    function findMaxDepth(node){
+      if (!node.left && !node.right){
+        return 1
+      }
+      if (node.left === null) return findMaxDepth(node.left) + 1
+      if (node.right === null) return findMaxDepth(node.right) +1
+      return (
+        Math.max(findMaxDepth(node.left), findMaxDepth(node.right)) + 1
+      )
+     }
+  
+     return findMaxDepth(this.root)
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    let result = 0;
 
+    function maxSumHelper(node) {
+      if (node === null) return 0;
+      const leftSum = maxSumHelper(node.left);
+      const rightSum = maxSumHelper(node.right);
+      result = Math.max(result, node.val + leftSum + rightSum);
+      console.log("result: ", result)
+      return Math.max(0, leftSum + node.val, rightSum + node.val);
+    }
+
+    maxSumHelper(this.root);
+    return result;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
-
+    if (!this.root) return null;
+    let currLowest = 999;
+    function findNextLarger(currNode){
+      //extra base case: if currNode.val is not greater than lowerbound, return the next lowest
+      //which is stored in a variable in case of the event
+      if(currNode.val <= lowerBound){
+        console.log(`oops, ${currNode.val} is greater than or equal to ${lowerBound}, using ${currLowest} instead`)
+        return currLowest
+      } 
+      if (!currNode.left && !currNode.right) return currNode.val
+      if(!currNode.left){
+        currLowest = Math.min(currNode.val, findNextLarger(currNode.right))
+        return currLowest
+      } 
+      if(!currNode.right){
+        currLowest =  Math.min(currNode.val, findNextLarger(currNode.left))
+        return currLowest
+      } 
+      else{
+        currLowest =  Math.min(currNode.val, findNextLarger(currNode.left), findNextLarger(currNode.right))
+        return currLowest
+      }
+     
+    }
+    
+    let final = findNextLarger(this.root)
+    if (final == 999) return null
+    else return final
+    
   }
 
   /** Further study!
@@ -71,5 +136,24 @@ class BinaryTree {
     
   }
 }
+emptyTree = new BinaryTree();
+
+  // build small tree;
+  let smallLeft = new BinaryTreeNode(5);
+  let smallRight = new BinaryTreeNode(5);
+  let smallRoot = new BinaryTreeNode(6, smallLeft, smallRight);
+  smallTree = new BinaryTree(smallRoot);
+
+  // build large tree
+  let node6 = new BinaryTreeNode(1);
+  let node5 = new BinaryTreeNode(1);
+  let node4 = new BinaryTreeNode(2);
+  let node3 = new BinaryTreeNode(3, node4, node6);
+  let node2 = new BinaryTreeNode(5, node3, node5);
+  let node1 = new BinaryTreeNode(5);
+  let root = new BinaryTreeNode(6, node1, node2);
+  largeTree = new BinaryTree(root);
+
+  console.log(largeTree.maxSum())
 
 module.exports = { BinaryTree, BinaryTreeNode };
